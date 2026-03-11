192bc6740d63c93a90dda78518bee69d5dc9dbba
youzhiwang <2277071555@qq.com>
2026-03-11 11:43:32 +0800

    fix: 添加插件注释

--- plugins/autoSetComponentsScriptName.ts
+++ plugins/autoSetComponentsScriptName.ts
@@ -1,6 +1,7 @@
 import { Plugin } from 'vite';
 import path from 'path';
-
+// 在每个页面中script中添加name属性 用于路由缓存，
+// 此插件就是解放defineOptions 改用script中的name属性去缓存页面
 const autoSetScriptName = (): Plugin => {
   return {
     name: 'auto-set-component-name',
@@ -17,15 +18,12 @@ const autoSetScriptName = (): Plugin => {
         if (code.includes('defineOptions') || /<script[^>]*\s+name=/.test(code)) {
           return code;
         }
-
         // 匹配原始 <script setup> 标签
         const scriptSetupRegex = /<script\s+setup\s*([^>]*?)>/i;
         const match = code.match(scriptSetupRegex);
-
         if (match) {
           const fullTag = match[0];
           const attributes = match[1] || '';
-          
           // 添加 name 属性
           let newTag;
           if (attributes.trim()) {
@@ -33,10 +31,6 @@ const autoSetScriptName = (): Plugin => {
           } else {
             newTag = `<script setup name="${componentName}">`;
           }
-          
-          console.log('原始标签:', fullTag);
-          console.log('新标签:', newTag);
-          
           return code.replace(fullTag, newTag);
         }
       }
